1f57b7bccc273bc6741efd5615dcb1ee1445a7dd
hung0920 <hungooso4413@gmail.com>
2023-02-11 21:37:58 +0700

    Move_Image

--- src/Screen/Home.js
+++ src/Screen/Home.js
@@ -1,17 +1,34 @@
-import { View, Text, Pressable, Image, StyleSheet } from "react-native";
+import { View, Text, Pressable, Image, StyleSheet, Animated } from "react-native";
+import { useEffect } from "react";
 
 export default function HomeScreen(props) {
+
     const navigation = props.navigation;
+
+    const fadeIn = new Animated.Value(-200);
+
     const changeScreen = (screen) => {
         navigation.navigate(screen);
     };
 
+    useEffect(() => {
+        Animated.timing(fadeIn, {
+            toValue: 0,
+            duration: 3000,
+            useNativeDriver: true,
+        }).start();
+    }, []);
+
     return (
-        <View style={{alignItems: 'center'}}>
-            <Image 
-                style={styles.img}
-                source={{uri: 'https://reactjs.org/logo-og.png'}}
-            />
+        <View style={{ alignItems: 'center' }}>
+            <Animated.View style={{
+                transform: [{ translateX: fadeIn }]
+            }}>
+                <Image style={{width: 60,
+                height: 60,
+                borderRadius: 30}}
+                source={{ uri: 'https://reactjs.org/logo-og.png' }} />
+            </Animated.View>
             <Pressable style={styles.pressable} onPress={() => changeScreen("Info")}>
                 <Text style={styles.text}>Info</Text>
             </Pressable>
@@ -23,13 +40,13 @@ export default function HomeScreen(props) {
 }
 
 const styles = StyleSheet.create({
-    img:{
+    img: {
         width: 60,
         height: 60,
         borderRadius: 30,
-        marginVertical: 16
+        marginVertical: 16,
     },
-    pressable:{
+    pressable: {
         backgroundColor: '#40d6c1',
         width: 90,
         borderRadius: 5,
